02ebaef2afcaa8fe411240e9c104d3397cb759a6
Unknown <pawin.taechoyotin@gmail.com>
2018-03-01 03:16:29 +0700

    Add pca processing

--- main.py
+++ main.py
@@ -3,11 +3,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return 'Send data with "requests.post( )" with python'
 
-@app.route('/process')
-def process():
-  return 'process'
+@app.route('/process', methods=['GET','POST'])
+def processCube():
+    packedData = request.data
+    imageCube = pickle.loads( packedData )
+    if( isinstance(imageCube, np.ndarray) ):
+      # Keep image dimensions
+      bands, row, column = imageCube.shape
+      # tranform image cube to image matrix
+      imageMatrix = []
+      for i in range(bands):
+        imageMatrix.append(imageCube[i].flatten())
+
+      imageMatrix = np.array( imageMatrix )
+
+      # perform pca
+      eigenVector, pcList = pca( imageMatrix, row, column )
+
+      # Pack data for response
+      visibleImage = cv2.merge((imageCube[0],imageCube[1],imageCube[2]))
+      falseColorMap = cv2.merge((pcList[0],pcList[1],pcList[2]))
+
+      imageDict = dict()
+      imageDict['rgb'] = visibleImage
+      imageDict['falseColorMap'] = falseColorMap
+
+      imageDictPack = pickle.dumps( imageDict )
+      return imageDictPack
+      #mean = unpackedData.mean()
+      #return 'cube received: {}'.format( mean )
+    else:
+      return 'Data type ERROR'
+
 
 if __name__ == '__main__':
   app.run()
