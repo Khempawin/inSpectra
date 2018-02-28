2cc33e00b6b7829644258ccf14497da9822f8326
Unknown <pawin.taechoyotin@gmail.com>
2018-03-01 04:44:35 +0700

    Check runtime version

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return 'Hello, World!:{}'.format( sys.version_info[0] )
 
 if __name__ == '__main__':
   app.run()
