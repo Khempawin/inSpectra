b64796d12c5999026d9222852656efd4e77444c4
Unknown <pawin.taechoyotin@gmail.com>
2018-03-01 04:41:53 +0700

    Revert to tutorial

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-from flask import Flask, request
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def hello_world():
-  packedData = request.data
-  imageCube = pickle.loads( packedData )
-  if( isinstance(imageCube, np.ndarray) ):
-    # Keep image dimensions
-    bands, row, column = imageCube.shape
-    # tranform image cube to image matrix
-    imageMatrix = []
-    for i in range(bands):
-      imageMatrix.append(imageCube[i].flatten())
-      imageMatrix = np.array( imageMatrix )
-
-      # perform pca
-      eigenVector, pcList = pca( imageMatrix, row, column )
-
-      # Pack data for response
-      visibleImage = cv2.merge((imageCube[0],imageCube[1],imageCube[2]))
-      falseColorMap = cv2.merge((pcList[0],pcList[1],pcList[2]))
-
-      imageDict = dict()
-      imageDict['rgb'] = visibleImage.mean()
-      imageDict['falseColorMap'] = falseColorMap.mean()
-
-    return 'cube received: {}'.format( imageDict )
-  else:
-    return 'Data type ERROR'
+  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run()
