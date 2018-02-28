51c41714b957a882291dbd6b3358dce1df217682
Unknown <pawin.taechoyotin@gmail.com>
2018-03-01 04:54:29 +0700

    Check Architecture

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
+import platform
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!:{}.{}'.format( sys.version_info[0], sys.version_info[1] )
+  return 'Hello, World!:{}.{}, {}'.format( sys.version_info[0], sys.version_info[1], platform.architecture() )
 
 @app.route('/process/', methods=['GET','POST'])
 def process():
