1dc3e515096d0eb1e6a4dce672cf18f587ee606f
Unknown <pawin.taechoyotin@gmail.com>
2018-03-01 04:46:30 +0700

    add route

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello, World!:{}'.format( sys.version_info[0] )
 
+@app.route('/process/',methods=['GET','POST'])
+def process():
+	return 'Process'
+
+
 if __name__ == '__main__':
   app.run()
