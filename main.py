ab6d8453c5f31703a8cf67b56f4b004a2a9d305e
Unknown <pawin.taechoyotin@gmail.com>
2018-03-01 11:30:49 +0700

    Stage main

--- main.py
+++ main.py
@@ -3,16 +3,46 @@ from pca import pca
 import cv2
 import numpy as np
 import pickle
+import sys
+import platform
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!:{}.{}, {}'.format( sys.version_info[0], sys.version_info[1], platform.architecture() )
 
-@app.route('/process/', methods=['GET','POST'])
-def process():
-	return 'Process'
+@app.route('/process', methods=['GET','POST'])
+def processCube():
+    packedData = request.data
+    imageCube = pickle.loads( packedData )
+    if( isinstance(imageCube, np.ndarray) ):
+      # Keep image dimensions
+      bands, row, column = imageCube.shape
+      # tranform image cube to image matrix
+      imageMatrix = []
+      for i in range(bands):
+        imageMatrix.append(imageCube[i].flatten())
+
+      imageMatrix = np.array( imageMatrix )
+
+      # perform pca
+      eigenVector, pcList = pca( imageMatrix, row, column )
+
+      # Pack data for response
+      visibleImage = cv2.merge((imageCube[0],imageCube[1],imageCube[2]))
+      falseColorMap = cv2.merge((pcList[0],pcList[1],pcList[2]))
+
+      imageDict = dict()
+      imageDict['rgb'] = visibleImage
+      imageDict['falseColorMap'] = falseColorMap
+
+      imageDictPack = pickle.dumps( imageDict )
+      return imageDictPack
+      #mean = unpackedData.mean()
+      #return 'cube received: {}'.format( mean )
+    else:
+      return 'Data type ERROR'
 
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host='0.0.0.0')
